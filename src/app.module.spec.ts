379190417a36f74618974f32d9512eba4e782086
Nikolay Benlioglu <nikbenlioglu@gmail.com>
2026-04-16 00:54:39 +0300

    test: fix app module spec

--- src/app.module.spec.ts
+++ src/app.module.spec.ts
@@ -1,13 +1,17 @@
 import { MODULE_METADATA } from '@nestjs/common/constants';
+import { ThrottlerModule } from '@nestjs/throttler';
 import { AppModule } from './app.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { PromoCodesModule } from './promo-codes/promo-codes.module';
 
 describe('AppModule', () => {
-  it('imports the Prisma and promo codes modules', () => {
-    expect(Reflect.getMetadata(MODULE_METADATA.IMPORTS, AppModule)).toEqual([
-      PrismaModule,
-      PromoCodesModule,
-    ]);
+  it('imports the throttler, Prisma, and promo codes modules', () => {
+    expect(Reflect.getMetadata(MODULE_METADATA.IMPORTS, AppModule)).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({ module: ThrottlerModule }),
+        PrismaModule,
+        PromoCodesModule,
+      ]),
+    );
   });
 });
